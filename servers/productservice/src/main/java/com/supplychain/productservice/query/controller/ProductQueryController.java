8a0877b7c1a99f4dea7f977fe8629101583a54eb
TheKIDends <chaukhanh0605@gmail.com>
2024-10-31 06:47:34 +0700

    Add feature to retrieve all products in Product Service

--- servers/productservice/src/main/java/com/supplychain/productservice/query/controller/ProductQueryController.java
+++ servers/productservice/src/main/java/com/supplychain/productservice/query/controller/ProductQueryController.java
@@ -1,11 +1,14 @@
 package com.supplychain.productservice.query.controller;
 
 import com.supplychain.productservice.query.model.ProductResponseModel;
+import com.supplychain.productservice.query.query.GetAllProductsQuery;
 import com.supplychain.productservice.query.query.GetProductQuery;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("${api.base-path}")
@@ -19,5 +22,12 @@ public class ProductQueryController {
         GetProductQuery getProductQuery = new GetProductQuery(productId);
 
         return queryGateway.query(getProductQuery, ResponseTypes.instanceOf(ProductResponseModel.class)).join();
+    }
+
+    @GetMapping("${endpoint.get-all-products}")
+    public List<ProductResponseModel> getAllProducts() {
+        GetAllProductsQuery getAllProductsQuery = new GetAllProductsQuery();
+
+        return queryGateway.query(getAllProductsQuery, ResponseTypes.multipleInstancesOf(ProductResponseModel.class)).join();
     }
 }
--- servers/productservice/src/main/java/com/supplychain/productservice/query/projection/ProductProjection.java
+++ servers/productservice/src/main/java/com/supplychain/productservice/query/projection/ProductProjection.java
@@ -3,11 +3,15 @@ package com.supplychain.productservice.query.projection;
 import com.supplychain.productservice.command.data.Product;
 import com.supplychain.productservice.command.repository.ProductRepository;
 import com.supplychain.productservice.query.model.ProductResponseModel;
+import com.supplychain.productservice.query.query.GetAllProductsQuery;
 import com.supplychain.productservice.query.query.GetProductQuery;
 import org.axonframework.queryhandling.QueryHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class ProductProjection {
@@ -20,5 +24,19 @@ public class ProductProjection {
         Product product = productRepository.getById(getProductQuery.getProductId());
         BeanUtils.copyProperties(product, model);
         return model;
+    }
+
+    @QueryHandler
+    public List<ProductResponseModel> handle(GetAllProductsQuery getAllProductsQuery) {
+        List<Product> productList = productRepository.findAll();
+        List<ProductResponseModel> modelList = new ArrayList<>();
+
+        productList.forEach(product -> {
+            ProductResponseModel model = new ProductResponseModel();
+            BeanUtils.copyProperties(product, model);
+            modelList.add(model);
+        });
+
+        return modelList;
     }
 }
