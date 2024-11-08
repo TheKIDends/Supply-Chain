e12f378657534e88b62aa15459b42fd455c04b72
TheKIDends <chaukhanh0605@gmail.com>
2024-11-08 23:41:11 +0700

    Update Front-end

---
+++ client/web-app/src/page/user/customer/homePage/index.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import './style.scss'
+import {Breadcrumb} from "antd";
+
+const HomePage = () => {
+    const [count, setCount] = useState(0);
+
+    function incrementCount() {
+        return count + 1;
+    }
+
+    function handleClick() {
+        // setCount((a) => { return a + 1});
+        // setCount((a) => { return a + 1});
+        // setCount((a) => { return a + 1});
+        setCount(incrementCount);
+        setCount(incrementCount);
+        setCount(incrementCount);
+    }
+
+    return (
+        <>
+            <Breadcrumb
+                items={[
+                    {
+                        title: 'Home',
+                    },
+                    {
+                        title: 'List',
+                    },
+                    {
+                        title: 'App',
+                    },
+                ]}
+                style={{
+                    // margin: '16px 0',
+                }}
+            />
+            HomePage
+            {/*<button onClick={handleClick} style={{width:"100px"}}>{count}</button>*/}
+        </>
+    );
+}
+
+export default HomePage;
